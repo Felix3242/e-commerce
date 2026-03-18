888757223ff1646e7458723f1f793faee80a5647
Felix Gao <llai92641@gmail.com>
2026-03-17 23:26:17 -0400

    fixed navbar items

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -29,22 +29,24 @@ const Navbar = () => {
 
         </ul>
 
-        <div className='flex item-center gap-6'>
+        <div className='flex items-center gap-6'>
             <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
             <div className='group relative'>
                 <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
-                <div className='group-hover:block hidden absolute dropdwon-menu right-0 pt-4'>
-                    <div className='flex flex-col gap-2 w-36 py-3 px-5 big-slate-100 text-gray-500 rounded'>
-                        <p className='cursor-pointer hover:text-black'>My Profile</p>
-                        <p className='cursor-pointer hover:text-black'>Orders</p>
-                        <p className='cursor-pointer hover:text-black'>Logout</p>
-                    </div>
+                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                            <p className='cursor-pointer hover:text-black'>My Profile</p>
+                            <p className='cursor-pointer hover:text-black'>Orders</p>
+                            <p className='cursor-pointer hover:text-black'>Logout</p>
+                        </div>
                 </div>
-                <Link to='/cart' className='relative'>
-                    <img src={assets.cart_icon} className='w-5 cursor-pointer' alt="" />
-                </Link>
             </div>
+            <Link to='/cart' className='relative'>
+                <img className='w-5 min-w-5' src={assets.cart_icon} alt="" />
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+            </Link>
+            <img src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
         </div>
     </div>
   )
