a600e38805c576c4e9b5b42999385c48f9329424
Felix Gao <llai92641@gmail.com>
2026-03-18 23:21:07 -0400

    latest products

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import { useContext } from 'react'
+import React, { useEffect } from 'react'
+import { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
+    const [latestProducts, setLatestProducts] = useState([]);
 
-    console.log(products);
+    useEffect(() => {
+        setLatestProducts(products.slice(0, 10));
+    }, [])
 
   return (
-    <div>
-        
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+            <p className='w-3/4 mx-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+            </p>
+        </div>
+
+    
     </div>
   )
 }
