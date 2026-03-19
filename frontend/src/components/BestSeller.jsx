25b13535e48ef3a703c590a4454923c8b4842e59
Felix Gao <llai92641@gmail.com>
2026-03-18 23:43:10 -0400

    finished best sellers component

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const BestSeller = () => {
 
@@ -8,7 +9,7 @@ const BestSeller = () => {
     const [bestSeller, setBestSeller] = useState([]);
 
     useEffect(() => {
-        const bestProduct = products.filter((item)=>(item.bestSeller === true))
+        const bestProduct = products.filter((item)=>(item.bestseller === true))
         setBestSeller(bestProduct.slice(0,5));
     }, [])
 
@@ -21,7 +22,13 @@ const BestSeller = () => {
             </p>
         </div>
 
-        
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+                bestSeller.map((item,index) => (
+                    <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+                ))
+            }
+        </div>
     </div>
   )
 }
