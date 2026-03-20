ae17dba19135882f36db7c1500e7a64fa42f088a
Felix Gao <llai92641@gmail.com>
2026-03-19 22:48:13 -0400

    product sort

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets'
+import Title from '../components/Title';
 
 const Collection = () => {
 
@@ -45,6 +46,24 @@ const Collection = () => {
           </div>
         </div>
       </div>
+
+      {/* Right Side */}
+      <div className='flex-1'>
+
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+          <Title text1={'ALL'} text2={'COLLECTIONS'} />
+          {/* Product Sort */}
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relavent">Sort by: Relavent</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
+        </div>
+
+        
+
+      </div>
+
     </div>
   )
 }
