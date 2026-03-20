14a747fdf388e0d5ed159b47b96e07945ed387e1
Felix Gao <llai92641@gmail.com>
2026-03-19 22:40:55 -0400

    sub categories

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -26,6 +26,21 @@ const Collection = () => {
             </p>
           </div>
         </div>
+        {/* Sub Category Filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>TYPE</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'>
+              <input className='w-3t' type="checkbox" value={'Topwear'} /> Topwear 
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3t' type="checkbox" value={'Bottomwear'} /> Bottomwear
+            </p>
+            <p className='flex gap-2'>
+              <input className='w-3t' type="checkbox" value={'Winterwear'} /> Winterwear
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )
