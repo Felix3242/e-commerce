cbbee8c86ed7c54e04a9db83d8cb68f1effbd8ce
Felix Gao <llai92641@gmail.com>
2026-03-20 23:43:38 -0400

    quantities are proeprly recorded in cart

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Cart = () => {
+
+  const { productsData, currency, cartItems } = useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  },[cartItems])
+
   return (
-    <div>Cart</div>
+    <div className='border-t pt-14'>
+      
+      
+
+    </div>
   )
 }
 
