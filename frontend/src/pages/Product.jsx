d172893b6c972388279c9424352d51b9cb2e56ce
Felix Gao <llai92641@gmail.com>
2026-03-20 22:48:50 -0400

    sizes highlight

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -9,6 +9,7 @@ const Product = () => {
   const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = () => {
     products.map((item) => {
@@ -65,7 +66,13 @@ const Product = () => {
             <p>Select Size</p>
             <div className='flex gap-2'>
               {productData.sizes.map((item, index) => (
-                <button key={index} className={`border py-2 px-4 bg-gray-100`}>{item}</button>
+                <button
+                  onClick={() => setSize(item)}
+                  key={index}
+                  className={`border py-2 px-4 bg-gray-100 ${item === size ? "border-orange-500" : ""}`}
+                >
+                  {item}
+                </button>
               ))}
             </div>
           </div>
