6b7bbe5081c9a8afe0c32b58a255c6122612c65d
Felix Gao <llai92641@gmail.com>
2026-03-21 13:11:45 -0400

    removing items works

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -52,6 +52,12 @@ const ShopContextProvider = (props) => {
     return totalCount;
   }
 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  }
+
   const value = {
     products,
     currency,
@@ -63,6 +69,7 @@ const ShopContextProvider = (props) => {
     cartItems,
     addToCart,
     getCartCount,
+    updateQuantity
   };
 
   return (
