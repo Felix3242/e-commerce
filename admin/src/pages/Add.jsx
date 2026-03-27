f2acc5a15cd283d747fe9ccaa0112a20ef8b0d1b
Felix Gao <llai92641@gmail.com>
2026-03-26 23:13:38 -0400

    styled input fields select box and text areas

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -3,29 +3,32 @@ import { assets } from '../assets/assets'
 
 const Add = () => {
   return (
-    <form>
+    <form className='flex flex-col w-full items-start gap-3'>
       <div>
-        <p>Upload Image</p>
-        <div>
+        <p className='mb-2'>Upload Image</p>
+        <div className='flex gap-2'>
           <label htmlFor="image1">
-            <img className='w-20' src={assets.upload_area} alt="" />
+            <img className='cursor-pointer w-20' src={assets.upload_area} alt="" />
             <input type="file" id="image1" hidden />
           </label>
           <label htmlFor="image2">
-            <img className='w-20' src={assets.upload_area} alt="" />
+            <img className='cursor-pointer w-20' src={assets.upload_area} alt="" />
             <input type="file" id="image2" hidden />
           </label>
           <label htmlFor="image3">
-            <img className='w-20' src={assets.upload_area} alt="" />
+            <img className='cursor-pointer w-20' src={assets.upload_area} alt="" />
             <input type="file" id="image3" hidden />
           </label>
           <label htmlFor="image4">
-            <img className='w-20' src={assets.upload_area} alt="" />
+            <img className='cursor-pointer w-20' src={assets.upload_area} alt="" />
             <input type="file" id="image4" hidden />
           </label>
         </div>
       </div>
-        
+      <div>
+        <p>Product name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required />
+      </div>
     </form>
   )
 }
