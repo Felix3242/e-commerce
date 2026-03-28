cbcc4c4ba271947fcc63168d65520ac80458b9c0
Felix Gao <llai92641@gmail.com>
2026-03-28 14:14:26 -0400

    created list table title

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -27,14 +27,14 @@ const List = () => {
   return (
     <>
       <p className="mb-2">All Products List</p>
-      <div>
+      <div className="flex flex-col gap-2">
         {/* List Table Title */}
-        <div>
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
           <b>Image</b>
           <b>Name</b>
           <b>Category</b>
           <b>Price</b>
-          <b>Action</b>
+          <b className="text-center">Action</b>
         </div>
       </div>
     </>
