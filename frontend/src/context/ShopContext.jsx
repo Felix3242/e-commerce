81cd69890e270a454333cdb74be07932c24034b1
Felix Gao <llai92641@gmail.com>
2026-03-28 15:28:28 -0400

    Updated ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,7 +1,7 @@
-import { createContext, useState } from "react";
-import { products } from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const ShopContext = createContext();
@@ -10,15 +10,15 @@ const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const [products, setProducts] = useState([]);
   const navigate = useNavigate();
 
   const addToCart = async (itemId, size) => {
-
     if (!size) {
-      toast.error('Select Product Size');
+      toast.error("Select Product Size");
       return;
     }
 
@@ -34,10 +34,8 @@ const ShopContextProvider = (props) => {
       cartData[itemId] = {};
       cartData[itemId][size] = 1;
     }
-
     setCartItems(cartData);
-
-  }
+  };
 
   const getCartCount = () => {
     let totalCount = 0;
@@ -47,18 +45,20 @@ const ShopContextProvider = (props) => {
           if (cartItems[items][item] > 0) {
             totalCount += cartItems[items][item];
           }
-        // eslint-disable-next-line no-unused-vars
-        } catch (error) { /* empty */ }
+          // eslint-disable-next-line no-unused-vars
+        } catch (error) {
+          /* empty */
+        }
       }
     }
     return totalCount;
-  }
+  };
 
   const updateQuantity = async (itemId, size, quantity) => {
     let cartData = structuredClone(cartItems);
     cartData[itemId][size] = quantity;
     setCartItems(cartData);
-  }
+  };
 
   const getCartAmount = () => {
     let totalAmount = 0;
@@ -69,12 +69,32 @@ const ShopContextProvider = (props) => {
           if (cartItems[items][item] > 0) {
             totalAmount += cartItems[items][item] * itemInfo.price;
           }
-        // eslint-disable-next-line no-unused-vars
-        } catch (error) { /* empty */ }
+          // eslint-disable-next-line no-unused-vars
+        } catch (error) {
+          /* empty */
+        }
       }
     }
     return totalAmount;
-  }
+  };
+
+  const getProductsData = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      if (response.data.success) {
+        setProducts(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getProductsData();
+  }, []);
 
   const value = {
     products,
