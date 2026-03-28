71cc08b6a0087d20f2e08efd4f34f57a6d4966cc
Felix Gao <llai92641@gmail.com>
2026-03-28 15:52:09 -0400

    Updated ShopContext.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import axios from "axios";
 import { toast } from "react-toastify";
@@ -43,6 +43,12 @@ const Login = () => {
       toast.error(error.message);
     }
   };
+
+  useEffect(() => { 
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
 
   return (
     <form
