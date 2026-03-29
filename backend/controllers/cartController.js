ca3bd2ffdc09db019b471a4ab2b400877445ff2b
Felix Gao <llai92641@gmail.com>
2026-03-28 22:26:48 -0400

    Added addToCart logic to cartController.js

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,7 +1,32 @@
 // add products to user cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId, size } = req.body;
 
-// update  user cart
+    const userData = await userModel.findById(userId);
+    let cartData = await userData.cartData;
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+
+    res.json({ success: true, message: "Added to cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// update user cart
 const updateCart = async (req, res) => {};
 
 // get user cart data
