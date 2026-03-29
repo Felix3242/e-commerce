94fc582bdf5b2a82a83113f05c55283ffdd7c393
Felix Gao <llai92641@gmail.com>
2026-03-29 12:42:41 -0400

    Created allOrders function in orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,7 +32,15 @@ const placeOrderStripe = async (req, res) => {};
 const placeOrderRazorpay = async (req, res) => {};
 
 // All Orders data for Admin Panel
-const allOrders = async (req, res) => {};
+const allOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // User Order Data for Frontend
 const userOrders = async (req, res) => {
