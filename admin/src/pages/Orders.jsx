55537beb043f756f686039d3422b0b7deb20ba2b
Felix Gao <llai92641@gmail.com>
2026-03-29 13:13:47 -0400

    Set default value for order status in Orders component

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -40,25 +40,24 @@ const Orders = ({ token }) => {
       <h3>Order Page</h3>
       <div>
         {orders.map((order, index) => (
-          <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700' key={index}>
-            <img className='w-12' src={assets.parcel_icon} alt="" />
+          <div
+            className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700"
+            key={index}
+          >
+            <img className="w-12" src={assets.parcel_icon} alt="" />
             <div>
               <div>
                 {order.items.map((item, index) => {
                   if (index === order.items.length - 1) {
                     return (
-                      <p
-                        className="py-0.5" 
-                        key={index}>
+                      <p className="py-0.5" key={index}>
                         {" "}
                         {item.name} x {item.quantity} <span>{item.size}</span>
                       </p>
                     );
                   } else {
                     return (
-                      <p 
-                        className="py-0.5"
-                        key={index}>
+                      <p className="py-0.5" key={index}>
                         {" "}
                         {item.name} x {item.quantity} <span>{item.size}</span> ,
                       </p>
@@ -66,7 +65,9 @@ const Orders = ({ token }) => {
                   }
                 })}
               </div>
-              <p className="mt-3 mb-2 font-medium">{order.address.firstName + " " + order.address.lastName}</p>
+              <p className="mt-3 mb-2 font-medium">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
               <div>
                 <p>{order.address.street + ","}</p>
                 <p>
@@ -82,18 +83,22 @@ const Orders = ({ token }) => {
               <p>{order.address.phone}</p>
             </div>
             <div>
-              <p className="text-sm sm:text-[15px]">Items : {order.items.length}</p>
-              <p className='mt-3'>Method : {order.paymentMethod}</p>
-              <p>Payment : {order.payment ? 'Done' : 'Pending'}</p>
+              <p className="text-sm sm:text-[15px]">
+                Items : {order.items.length}
+              </p>
+              <p className="mt-3">Method : {order.paymentMethod}</p>
+              <p>Payment : {order.payment ? "Done" : "Pending"}</p>
               <p>Date : {new Date(order.date).toLocaleDateString()}</p>
             </div>
-            <p className='text-sm sm:text-[15px]'>{currency} {order.amount}</p>
-            <select className="p-2 font-semibold">
+            <p className="text-sm sm:text-[15px]">
+              {currency} {order.amount}
+            </p>
+            <select value={order.status} className="p-2 font-semibold">
               <option value="Order Placed">Order Placed</option>
               <option value="Packing">Packing</option>
               <option value="Shipped">Shipped</option>
               <option value="Out for delivery">Out for delivery</option>
-              <option value="Delivered">Delivered</option>  
+              <option value="Delivered">Delivered</option>
             </select>
           </div>
         ))}
